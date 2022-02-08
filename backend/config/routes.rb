21608159f57a208bb66f17ed78039797162eb3a2
takuma <takuma012011@gmail.com>
2022-02-08 19:22:01 +0900

    [add] #2 メールの設定を追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+# ログイン機能のルーティング
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  # ログインユーザー取得のルーティング
+  namespace :auth do
+    resources :sessions, only: %i[index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
