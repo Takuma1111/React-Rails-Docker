2c7abcbfd3c991ed7d8561be71d4deae85f8e39c
takuma <takuma012011@gmail.com>
2022-02-13 14:15:30 +0900

    [add] #14 Homeページのデザインを作成

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,16 +2,16 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 import Skeleton from '@material-ui/lab/Skeleton';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
 // images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
 import HomeImage from '../images/restaurant-image.jpg';
 
-// ログイン時はHomeコンポーネントへ遷移するようにする
-// export const Home = () => {
-//     return <p>Homeページです</p>
-// }
-
+//スライドショーを自動でスライドさせるための設定
+const EnhancedSwipeableViews = autoPlay(SwipeableViews);
 
 
 //divタグに対してデザインを指定している 
@@ -20,6 +20,7 @@ const HeaderWrapper = styled.div`
   justify-content: flex-start;
   padding: 8px 32px;
 `;
+
 
 const MainLogoImage = styled.img`
   height: 90px;
@@ -35,9 +36,9 @@ const MainCover = styled.img`
 const HomesContentsList = styled.div`
   display: flex;
   justify-content: space-between;
+  padding: 220px;
   margin-bottom: 150px;
-  margin-right: 100px;
-  margin-left: 100px;
+
   display: -webkit-flex;
   -webkit-justify-content: space-between;
 `;
@@ -45,11 +46,48 @@ const HomesContentsList = styled.div`
 const HomesContentWrapper = styled.div`
   width: 450px;
   height: 300px;
+`;
+const ExplainContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+`;
+
+const ExplainContentsList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 150px;
+  margin-right: 300px;
+  margin-left: 300px;
+  display: -webkit-flex;
+  -webkit-justify-content: space-between;
+`;
+
+const ExplainContentsMessageBox = styled.div`
+  background-color: #A9A9A9;
+`;
+const ExplainContentsMessage = styled.p`
+  color: #FFFFFF;
+  font-size: 20px;
+  line-height: 1.8em;
+`;
+const ExplainContentsTitle = styled.h1`
+  color: #777777;
+  font-size: 100px;
+`;
+
+const ExplainContentsSubTitle = styled.h2`
+  color: #404040;
+  font-size: 46px;
+`;
+
+const SlideImageNode = styled.img`
+  width: 1000px;
+  height: 500px;
   padding: 48px;
 `;
 
 const HomesImageNode = styled.img`
-  width: 100%;
+  width: 80%;
 `;
 
 const MainText = styled.p`
@@ -62,18 +100,35 @@ const SubText = styled.p`
   font-size: 12px;
 `;
 
-
-//useEffectでレンダリング時に一回だけfetchRestaurantsと言う
-//APIを呼ぶための関数が呼ばれる
-//dispatchはreducerを通して間接的にstateを変更させる
 export const Home = () => {
   return (
     <Fragment>
-      <HeaderWrapper>
-        <MainLogoImage src={MainLogo} alt="main logo" />
-      </HeaderWrapper>
+
       <MainCoverImageWrapper>
-        <MainCover src={MainCoverImage} alt="main cover" />
+      <EnhancedSwipeableViews enableMouseEvents>
+      <SlideImageNode src={HomeImage} />
+      <SlideImageNode src={HomeImage} />
+      <SlideImageNode src={HomeImage} />
+    </EnhancedSwipeableViews>
+    <ExplainContentsList>
+      <ExplainContentWrapper>
+        <ExplainContentsTitle>Creative</ExplainContentsTitle>
+        <ExplainContentsSubTitle>What want you create?</ExplainContentsSubTitle>      
+        <MainText>イラスト・写真・音楽・映像</MainText>
+        <MainText>クリエイターの創作物を感じましょう</MainText>
+      </ExplainContentWrapper>
+      <ExplainContentWrapper>
+        <ExplainContentsMessageBox>
+          <div style={{ padding: '50px' }}>
+          <ExplainContentsMessage><h2>Photo · Music · Video</h2></ExplainContentsMessage>
+          <ExplainContentsMessage>Let's  post this!</ExplainContentsMessage>
+          <ExplainContentsMessage>I made a site that creators can contribute.
+                       You can post photos, music and videos on this site. Please choose the work you want to post and post. For heavy-sized files, loading of the site is delayed.
+                       The quality is not high, but please enjoy it.</ExplainContentsMessage>
+          </div>
+        </ExplainContentsMessageBox>
+      </ExplainContentWrapper>
+    </ExplainContentsList>
       </MainCoverImageWrapper>
       <HomesContentsList>
         {
