94e264cb6a08058cb08e1dee2cf3151d0b849922
takuma <takuma012011@gmail.com>
2022-02-13 15:24:11 +0900

    [add] #15 動画一覧ページに動画のサムネイルを表示

--- frontend/src/containers/Movies.jsx
+++ frontend/src/containers/Movies.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment,useReducer,useEffect } from 'react';
 import { Link } from "react-router-dom";
-  
+import styled from 'styled-components';
 import { fetchMovies } from '../api/movies';
 
 import {
@@ -8,6 +8,17 @@ import {
   moviesActionTypes,
   moviesReducer,
 } from '../reducers/movies';
+
+const MoviesContentsList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 50px;
+  margin-right: 50px;
+  margin-left:  50px;
+
+  display: -webkit-flex;
+  -webkit-justify-content: space-between;
+`;
 
 export const Movies = () => {
   const [state, dispatch] = useReducer(moviesReducer, initialState);
@@ -29,19 +40,19 @@ export const Movies = () => {
     <Fragment>
       
     <h1>動画のAPI情報一覧</h1>
+    <MoviesContentsList>       
     {
-state.moviesList.map((movie,index) =>
-<Link to={`/movies/${movie.id}`} key={index} style={{ textDecoration: 'none' }}>
+      state.moviesList.map((movie,index) =>
+        <Link to={`/movies/${movie.id}`} key={index} style={{ textDecoration: 'none' }}>
 
-<div>
- 
-  <p>{movie.name}</p>
-  <p>{movie.url}</p>
-</div>
-</Link>
-  )
-    
+        <div>
+          <p>{movie.name}</p>
+          <video src={movie.url}  width="320" height = "300" />
+        </div>
+        </Link>
+      )
     }
+  </MoviesContentsList>       
 </Fragment>
   )
 }
