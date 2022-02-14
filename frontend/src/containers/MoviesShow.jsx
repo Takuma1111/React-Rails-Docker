53ec7cca8544eece2c4a6f8c640adb07dbe9d21e
takuma <takuma012011@gmail.com>
2022-02-14 19:10:52 +0900

    [fix] #18 動画詳細ページのデザインを修正

--- frontend/src/containers/MoviesShow.jsx
+++ frontend/src/containers/MoviesShow.jsx
@@ -9,7 +9,30 @@ import {
   moviesReducer,
 } from '../reducers/movies';
 
+const MoviesContentsList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 150px;
+  padding-right: 10%;
+  padding-left: 10%;
+  display: -webkit-flex;
+  -webkit-justify-content: space-between;
+`;
 
+const Under = styled.h1 `
+  border-bottom: solid 2px gray;
+  margin-right: 20%;
+  margin-left: 20%;
+  font-family:inherit;
+  font-size: 35px;
+  font-weight: 700;
+  word-break: break-all;
+  color: #777777;
+`;
+
+const ExplainContent = styled.p `
+    padding-top : 15%;
+`;
 
 export const MovieShow = ({
     match
@@ -35,15 +58,22 @@ export const MovieShow = ({
   console.log(state.moviesList)
   return (
     <Fragment>
-        <h1>動画</h1>
+        <center><h2><Under>{state.moviesList.name}</Under></h2></center>
+        <MoviesContentsList>       
         {
-          <center>
-            <div>
-              <p>{state.moviesList.name}</p>
-              <ReactPlayer url={state.moviesList.url} id="MainPlay" muted playing loop controls={true} />
-            </div>
-          </center>
+              <ReactPlayer url={state.moviesList.url} width="80%" height = "650px" id="MainPlay" muted playing loop controls={true} />
         }
-5    </Fragment>
+        {
+          
+            <div>
+               <center><h2>説明</h2>
+              <ExplainContent>
+                <p>{state.moviesList.text}</p>
+              </ExplainContent>
+              </center>
+            </div>
+        }
+        </MoviesContentsList>       
+   </Fragment>
   )
 }
